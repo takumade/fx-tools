be343b8f4709101364aaf3d55585128258ec85c7
Takunda Madechangu <takundam@sonity.net>
2023-01-09 00:26:34 +0200

    Add argument parser

--- fx-tools.py
+++ fx-tools.py
@@ -6,10 +6,54 @@
 # 2. Process the commands
 # 3. Show the result to the user
 # Prompt: fx-tools>
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description ="Tools for trading Deriv's Synthentic Indices. Written in Python")
+    parser.add_argument('-i', '--interactive',
+                        action ='store_true',
+                        help ='Run in interactive mode')
+    args = parser.parse_args()
+    
+    if (args.interactive):
+        interactive()
+    else:
+        parser.print_help()
 
 
 
-def getInput():
+
+def get_input():
     user_input = input("fx-tools> ")
     return user_input
 
+
+def interactive():
+    print("""
+          
+███████╗██╗  ██╗  ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+██╔════╝╚██╗██╔╝  ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+█████╗   ╚███╔╝█████╗██║   ██║   ██║██║   ██║██║     ███████╗
+██╔══╝   ██╔██╗╚════╝██║   ██║   ██║██║   ██║██║     ╚════██║
+██║     ██╔╝ ██╗     ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+╚═╝     ╚═╝  ╚═╝     ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+                                                           
+       =[ fx-tools v1.0.0                                  ]                                                          
++ -- --=[ 5 tools                                          ]   
+    """)
+    
+    user_input = ""
+    exit_list = ["exit", "bye", "XXX", "exit()"]
+    
+    try:
+        while exit_list.count(user_input) == 0:
+            user_input = get_input()
+        
+        print("bye bye!")
+    except KeyboardInterrupt:
+        print("bye bye!")
+
+    
+if __name__ == '__main__':
+    parse_arguments()
